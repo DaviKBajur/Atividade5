cd65dc3f11b71d944d777cf04171445e6ab65713
DaviKBajur <davibajur@gmail.com>
2024-05-28 09:57:27 -0300

    Update Atividade5-q2.c

--- Atividade5-q2.c
+++ Atividade5-q2.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+#ifdef TESTADOR
+  #define PRINTF(format, ...) fprintf(stderr, format, __VA_ARGS__)
+#else
+  #define PRINTF(format, ...) printf(format, __VA_ARGS__)
+#endif
+
+
+
 #define sucesso 0
 #define Dias_no_mes 30
 #define Dias_no_ano 365
@@ -13,12 +21,14 @@ int main(int argv, char ** argc){
 
     int anos, meses, dias, diastotais;
 
-    printf("diga quantos anos dias e meses voce possui (ex: 18/05/20, ou seja 20 anos 5 meses e 18 dias)\n : ");
-    scanf("%d/%d/%d", &dias, &meses, &anos);
+    printf("diga quantos anos meses e dias voce possui (ex: 18/05/20, ou seja 18 anos 5 meses e 20 dias)\n : ");
+    scanf("%d/%d/%d", &anos, &meses, &dias);
 
     diastotais = CalcularDiasTotais(dias, meses, anos);
 
-    printf("%d dias %d meses e %d anos equivalem a %d dias",dias, meses, anos, diastotais);
+    printf("%d dias %d meses e %d anos equivalem a ",dias, meses, anos);
+    PRINTF("%d", diastotais);
+    printf(" dias\n");
 
     return sucesso;
 }
