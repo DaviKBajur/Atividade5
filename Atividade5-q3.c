350f2372507f80feaef0a3c8da986f43905eee40
DaviKBajur <davibajur@gmail.com>
2024-05-28 11:32:43 -0300

    Update Atividade5-q3.c

--- Atividade5-q3.c
+++ Atividade5-q3.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+#ifdef TESTADOR
+  #define PRINTF(format, ...) fprintf(stderr, format, __VA_ARGS__)
+#else
+  #define PRINTF(format, ...) printf(format, __VA_ARGS__)
+#endif
+
 #define sucesso 0
 #define Segundos_Em_Hora 3600
 #define Segundos_Em_Minuto 60
@@ -14,7 +20,7 @@ void CorverterSegundosEmHorasEMinutos(int segundos_totais){
     minutos = segundos / Segundos_Em_Minuto;
     segundos = segundos % Segundos_Em_Minuto;
 
-    printf("| Horas: %d | Minutos: %d | Segundos: %d |\n",horas, minutos, segundos );
+    PRINTF("%02d:%02d:%02d", horas, minutos, segundos);
 }
 
 int main(int argv, char ** argc){
